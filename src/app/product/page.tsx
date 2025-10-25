b3008bc66b3028218da2e723d6301cec3d197b64
ilhambonardoo <ilhambonardo123@gmail.com>
2025-10-26 00:15:38 +0700

    Membuat intercepting route dimana jika kita mengklik sebuah card product maka tidak akan berpindah tampilan namun dia akan menampilkan tampilan modal.

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,26 +1,13 @@
 import Link from "next/link";
+import getData from "@/services/products";
+
 type ProductPage = {
   params: { slug: string[] };
 };
 
-async function getData() {
-  // const res = await fetch("https://fakestoreapi.com/products", {
-  //   cache: "no-store",
-  // });
-  const res = await fetch("http://localhost:3000/api/product", {
-    cache: "force-cache",
-    next: { tags: ["products"] },
-    // next: { revalidate: 10 },
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
-
 export default async function DetailProductPage({ params }: ProductPage) {
   const { slug } = params;
-  const products = await getData();
+  const products = await getData("http://localhost:3000/api/product");
   console.log(products);
   return (
     <>
@@ -38,37 +25,30 @@ export default async function DetailProductPage({ params }: ProductPage) {
       </h1> */}
         {products.data.length > 0 &&
           products.data.map((product: any) => (
-            <div
+            <Link
+              href={`/product/detail/${product.id}`}
               className="w-full max-w-sm bg-gray-400 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
               key={product.id}
             >
-              <Link href="#">
-                <img
-                  className="p-8 rounded-t-lg object-cover h-96 w-full"
-                  src={product.image}
-                  alt="product image"
-                />
-              </Link>
+              <img
+                className="p-8 rounded-t-lg object-cover h-96 w-full"
+                src={product.image}
+                alt="product image"
+              />
               <div className="px-5 pb-5">
-                <Link href="#">
-                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
-                    {product.title}
-                  </h5>
-                </Link>
-
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
+                  {product.title}
+                </h5>
                 <div className="flex items-center justify-between mt-3">
                   <span className="text-3xl font-bold text-gray-900 dark:text-white">
                     ${product.price}
                   </span>
-                  <Link
-                    href="#"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
+                  <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Add to cart
-                  </Link>
+                  </button>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
       </div>
       {slug && (
