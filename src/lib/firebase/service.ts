af65f98b82a7ac675c24c29f891776b8ced0b62f
ilhambonardoo <ilhambonardo123@gmail.com>
2025-10-26 21:48:28 +0700

    Membuat fitur register page berjalan yang dihubungkan ke database firebase. Disini saya menggunakan bcrypt untuk melindungi password pada saat disimpan di database.

--- src/lib/firebase/service.ts
+++ src/lib/firebase/service.ts
@@ -1,11 +1,15 @@
 import {
+  addDoc,
   collection,
   doc,
   getDoc,
   getDocs,
   getFirestore,
+  query,
+  where,
 } from "firebase/firestore";
 import app from "./init";
+import bcrypt from "bcrypt";
 
 const fireStore = getFirestore(app);
 
@@ -19,4 +23,39 @@ export async function retrieveDataById(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(fireStore, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+export async function register(data: {
+  fullname: string;
+  email: string;
+  password: string;
+  role?: string;
+}) {
+  const q = query(
+    collection(fireStore, "users"),
+    where("email", "==", data.email)
+  );
+
+  const snapshot = await getDocs(q);
+  const users = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (users.length > 0) {
+    return {
+      status: false,
+      statusCode: 400,
+      message: "Email already registered",
+    };
+  } else {
+    data.role = "admin";
+    data.password = await bcrypt.hash(data.password, 10);
+    try {
+      await addDoc(collection(fireStore, "users"), data);
+      return { status: true, statusCode: 200, message: "Register successful" };
+    } catch (error) {
+      return { status: false, statusCode: 400, message: "Register failed" };
+    }
+  }
 }
